dbd5ee14fede36abd490496397363d1c54396333
fmval <132489026+fmval@users.noreply.github.com>
2026-02-19 11:25:08 +0000

    ✨ feat(database): add management commands and populate_db script

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'octofit_tracker',
     'api',
     'rest_framework',
+    'djongo',
     'corsheaders',
 ]
 
